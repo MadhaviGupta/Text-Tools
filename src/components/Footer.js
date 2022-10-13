223c0b84160deda2de5356a6d22bd1bdc95045de
MadhaviGupta <madhavigupta1225@gmail.com>
2022-10-13 23:37:52 +0530

    test commit2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -36,7 +36,7 @@ export default function Footer(props) {
         >
           Made by
           <a href="mailto:madhavigupta1225@gmail.com" className="mx-1">
-            Madhavi
+            Madhavi Gupta 
           </a>
           using ReactJS
         </div>
